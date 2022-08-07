4cd0428b7138f18d36a8214ac2c82e4d2883a85d
Carlos Sousa <carlos.sousa@gmx.us>
2022-08-07 19:48:31 +0200

    REST API with GET, POST, DELETE and PUT methods for item class

--- code/app.py
+++ code/app.py
@@ -1,16 +1,57 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
 api = Api(app)
 
+items = []
+
+
 # Every resource is a class
-class Student(Resource):  # Inherits from Resource
+class Item(Resource):  # Inherits from Resource
     def get(self, name):
-        return {"student": name}
+        try:
+            return next(item for item in items if item["name"] == name)
+        except StopIteration:
+            return {"Error 507": "Item not found in the collection"}, 404
 
-api.add_resource(Student, "/student/<string:name>")
+    def post(self, name):
+        for item in items:
+            if item["name"] == name:
+                return {"Error 509": f"The item {name} is already in the collection. No actions will be taken"}, 400
 
-app.run(port=5000)
+        data = request.get_json()
 
+        item = {"name": name, "price": data["price"]}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        try:
+            item = next(item for item in items if item["name"] == name)
+            items.remove(item)
+            return item
+        except StopIteration:
+            return {"Error 507": "Item not found in the collection"}, 404
+
+    def put(self, name):
+        try:
+            item = next(item for item in items if item["name"] == name)
+            data = request.get_json()
+
+            item["price"] = data["price"]
+            return item, 200
+        except StopIteration:
+            return {"Error 507": "Item not found in the collection"}, 404
+
+
+class ItemList(Resource):
+    def get(self):
+        return {"items": items}
+
+
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+
+app.run(port=5000, debug=True)
